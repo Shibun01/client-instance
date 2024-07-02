519a807df735a49ffc59e0adba1b75758f43e85d
shiba-deo <shiba@mycareershapers.in>
2024-07-02 16:25:05 +0530

    Done

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -37,58 +37,62 @@ const Dashboard = () => {
     );
 
     return (
-        <>
-            <div className="Dashboard_Container">
-                <div className="Dashboard_Body_container">
-                    <div className="Dashboard_Search_container">
-                        <div className="Dashboard_Search_inner_container">
-                            <div className={`Dashboard_Search_input_container ${!isDesktop ? 'Desktop_Search_mobile_container' : ''}`}>
-                                <div className="Dashboard_Search_input">
-                                    <SearchIconSVG />
-                                    <input
-                                        type="text"
-                                        className="Dashboard_Search_text"
-                                        placeholder="Search"
-                                        value={searchQuery}
-                                        onChange={(e) => setSearchQuery(e.target.value)}
-                                    />
-                                </div>
-                                <div onClick={handleMapClick} className="Dashboard_Explore_container">
-                                    <ExploreSVG />
-                                </div>
+        <motion.div
+            className="Dashboard_Container"
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 30 }}
+            transition={{ duration: 0.6  }}
+        >
+            <div className="Dashboard_Body_container">
+                <div className="Dashboard_Search_container">
+                    <div className="Dashboard_Search_inner_container">
+                        <div className={`Dashboard_Search_input_container ${!isDesktop ? 'Desktop_Search_mobile_container' : ''}`}>
+                            <div className="Dashboard_Search_input">
+                                <SearchIconSVG />
+                                <input
+                                    type="text"
+                                    className="Dashboard_Search_text"
+                                    placeholder="Search"
+                                    value={searchQuery}
+                                    onChange={(e) => setSearchQuery(e.target.value)}
+                                />
+                            </div>
+                            <div onClick={handleMapClick} className="Dashboard_Explore_container">
+                                <ExploreSVG />
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div className="Dashboard_Shops_container">
-                        <div className="Dashboard_Shops_grid">
-                            {filteredShops?.map((shop) => (
-                                <div
-                                    key={shop._id}
-                                    className="Dashboard_Shops_card"
-                                    onClick={() => handleCardClick(shop._id)}
-                                >
-                                    <div className="shop-image">
-                                        {shop?.images?.map((image, index) => (
-                                            <div className="shop-image" key={index}>
-                                                <img src={image} alt={`Shop Image ${index + 1}`} />
-                                            </div>
-                                        ))}
-                                    </div>
-                                    <div className="shop-details">
-                                        <h1 className="shop-title">{shop.name}</h1>
-                                        <h1 className="shop-subtitle">{shop.description}</h1>
-                                        <div className="shop-rating">
-                                            <StarSVG />
-                                            <p className="rating-text">
-                                                <span className="rating-value">{shop.rating}</span>
-                                                <span className="rating-reviews"> ({shop.rating_number} reviews)</span>
-                                            </p>
+                <div className="Dashboard_Shops_container">
+                    <div className="Dashboard_Shops_grid">
+                        {filteredShops?.map((shop) => (
+                            <div
+                                key={shop._id}
+                                className="Dashboard_Shops_card"
+                                onClick={() => handleCardClick(shop._id)}
+                            >
+                                <div className="shop-image">
+                                    {shop?.images?.map((image, index) => (
+                                        <div className="shop-image" key={index}>
+                                            <img src={image} alt={`Shop Image ${index + 1}`} />
                                         </div>
+                                    ))}
+                                </div>
+                                <div className="shop-details">
+                                    <h1 className="shop-title">{shop.name}</h1>
+                                    <h1 className="shop-subtitle">{shop.description}</h1>
+                                    <div className="shop-rating">
+                                        <StarSVG />
+                                        <p className="rating-text">
+                                            <span className="rating-value">{shop.rating}</span>
+                                            <span className="rating-reviews"> ({shop.rating_number} reviews)</span>
+                                        </p>
                                     </div>
                                 </div>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
@@ -107,7 +111,7 @@ const Dashboard = () => {
                     </div>
                 </motion.div>
             )}
-        </>
+        </motion.div>
     );
 }
 
